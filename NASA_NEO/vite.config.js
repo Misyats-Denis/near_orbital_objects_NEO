deb18c1b33ab97ac77276028b9f1af6a6327ee2e
Misyats-Denis <misyatsdenis@gmail.com>
2023-07-08 10:44:04 +0300

    add: deploy workflow2

--- NASA_NEO/vite.config.js
+++ NASA_NEO/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/near_orbital_objects_NEO/"
+  base:"/NASA_NEO/"
 
 })
